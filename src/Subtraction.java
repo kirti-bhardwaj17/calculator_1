b58b8ec082f1c1e21f8739f00d9a2869a323fce3
kirti-bhardwaj17 <bhardwajkirti17@gmail.com>
2025-01-17 15:16:26 +0530

    subtraction code

---
+++ src/Subtraction.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Subtraction {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter first number");
+        int num1=sc.nextInt();
+        System.out.println("Enter second number");
+        int num2=sc.nextInt();
+        System.out.println("Enter third number");
+        int num3=sc.nextInt();
+        int res=num1-num2-num3;
+        System.out.println(res);
+    }
+}
